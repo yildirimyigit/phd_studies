863d54fb016e9a8b22b72481eab14c1e562a7eb7
yigit <yigyil@gmail.com>
2019-06-08 20:40:18 +0300

    data processing script. Model generation is complete. Processing the demonstrations is next

--- s3/python_ws/dme_irl_on_mc/process_data.py
+++ s3/python_ws/dme_irl_on_mc/process_data.py
@@ -4,31 +4,27 @@
 
 from utils import *
 import numpy as np
+from environment import Environment
+
+
+def generate_model():
+    env = Environment()
+    env.initialize_environment()
+
+    env.save_states('data/states.npy')
+    env.save_actions('data/actions.npy')
+    env.save_transitions('data/transitions.npy')
 
 
 # Loads previously recorded demonstrations to create trajectories
-def run(path='data/'):
+def generate_trajectories(path='data/'):
     demonstrations = []
     for i in range(9):
         demonstrations.append(np.load(path+'big_t'+str(i)+'.npy'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    # TODO compute states of trajectories and save them in trajectories.npy
 
 
 if __name__ == "__main__":
-	run()
+    generate_model()
+    # generate_trajectories()
