36b8e811e92eb80633b7821840920408d528c6a1
yigit <yigyil@gmail.com>
2019-06-08 20:39:17 +0300

    Environment is updated

--- s3/python_ws/dme_irl_on_mc/environment.py
+++ s3/python_ws/dme_irl_on_mc/environment.py
@@ -4,208 +4,80 @@
 import numpy as np
 from utils import *
 
+import gym
+from gym import wrappers
+
+vid_ep = 20
+
 
 class Environment(object):
-    def __init__(self):
+    def __init__(self, state_div=20, action_div=10):
+        self.state_div = state_div
+        self.action_div = action_div
+        self.action_space = (-1, 1)
+        self.state_space = {'x': (-1.2, 0.6), 'v': (-0.07, 0.07)}
         self.state_list = []    # list of State objects
         self.action_list = []   # list of Action objects
 
-    # actions array should start from -90 to +90 degrees thus if divided by 5:
-    # | -72 | -36 | 0 | 36 | 72 | -> 1/2-(1/10) + i*1/5
+    # actions array has to be in between -1 and +1 thus if divided by 5:
+    # | -0.8 | -0.4 | 0 | 0.4 | 0.8 |
     def initialize_actions(self):
         print('+ Environment.initialize_actions()')
-        change = 1.0 / self.action_div  # the beginning should be in the middle
+        action_interval = self.action_space[1]-self.action_space[0]
+        change = action_interval / self.action_div
         for i in range(self.action_div):
-            # it is multiplied with pi in order to give it in radians format
-            self.action_list.append(Action((-1/2.0 + change / 2.0 + i * change) * math.pi))
+            self.action_list.append(Action(self.action_space[0] + (0.5 + i) * change))
 
-    # thetas are also initialized the same way with the actions.
-    # only, they are divided in the range(0,360) degrees instead of (0,180)
     def initialize_states(self):
         print('+ Environment.initialize_states()')
-        human_change = 1.0 / self.theta_human_div
-        goal_change = 1.0 / self.theta_goal_div
+        x_interval = self.state_space['x'][1] - self.state_space['x'][0]
+        v_interval = self.state_space['v'][1] - self.state_space['v'][0]
+        x_change = x_interval / self.state_div
+        v_change = v_interval / self.state_div
+        for i in range(self.state_div):
+            x = self.state_space['x'][0] + (0.5 + i) * x_change
+            for j in range(self.state_div):
+                v = self.state_space['v'][0] + (0.5 + j) * v_change
+                self.state_list.append(State(x, v))
 
-        # discretizing the distances in logarithmic scale
-        current_goal_distance = min_goal_dist
-        max_human_dist = 3.21
-        max_goal_distance = self.calculate_max_distance()
+    # def initialize_transitions(self):
+    #     print('+ Environment.initialize_states()')
+    #     x_interval = self.state_space['x'][1] - self.state_space['x'][0]
+    #     v_interval = self.state_space['v'][1] - self.state_space['v'][0]
+    #     x_change = x_interval / self.state_div
+    #     v_change = v_interval / self.state_div
+    #     for i in range(self.state_div):
+    #         x = self.state_space['x'][0] + (0.5 + i) * x_change
+    #         for j in range(self.state_div):
+    #             v = self.state_space['v'][0] + (0.5 + j) * v_change
+    #             self.state_list.append(State(x, v))
 
-        while current_goal_distance < max_goal_distance:
-            for i in range(self.theta_goal_div):
-                tg_change = (-1/2.0 + goal_change/2.0 + i * goal_change) * 2*math.pi
-                current_human_dist = min_human_dist
-                while current_human_dist < max_human_dist:
-                    for j in range(self.theta_human_div):
-                        th_change = (-1 / 2.0 + human_change / 2.0 + j * human_change) * 2 * math.pi
-                        self.state_list.append(State(current_goal_distance, tg_change, current_human_dist, th_change))
-                    current_human_dist *= 2
-            current_goal_distance *= 2
-        # for s in self.state_list:
-        #     print_state(s)
-
-    def random_state(self):
-        return np.random.choice(self.state_list)
-
-    # This method returns the probability distribution on the state space which corresponds to the probabilities of
-    # the agent's being on each state when it takes the given action in given state.
-    # The angle 0 represents the front of the agent, and x-y axes are set according to angles. Thus in this
-    # case x represents vertical axis, and y represents horizontal axis. And sin&cos values are calculated
-    # accordingly
-    # left:-y, up:+x, right:+y, down:-x (according to the agent)
-    def transition(self, state, action):
-        dhx = state.dh * math.cos(state.th)
-        dhy = state.dh * math.sin(state.th)
-        dgx = state.dg * math.cos(state.tg)
-        dgy = state.dg * math.sin(state.tg)
-
-        # print(dgx, dgy, dhx, dhy)
-
-        # dgyn stands for new distance goal (y), the same for the rest of the variables as well
-        if abs(math.cos(action.middle_degree) - math.cos(state.tg)) > 1.0:
-            # it means that x dimensions of the action vector and distance_human vector is not the same
-            if dgx < 0:
-                dgxn = dgx - self.delta_distance / 2.0 * math.cos(action.middle_degree)
-            else:
-                dgxn = dgx + self.delta_distance / 2.0 * math.cos(action.middle_degree)
-        else:   # then they are on the same side. then a step towards that side decreases the distance
-            if dgx < 0:
-                dgxn = dgx + self.delta_distance / 2.0 * math.cos(action.middle_degree)
-            else:
-                dgxn = dgx - self.delta_distance / 2.0 * math.cos(action.middle_degree)
-
-        if abs(math.sin(action.middle_degree) - math.sin(state.tg)) > 1.0:
-            # they are not on the same side with y axis
-            dgyn = dgy - self.delta_distance / 2.0 * math.sin(action.middle_degree)
-        else:   # then they are on the same side. then a step towards that side decreases the distance
-            dgyn = dgy + self.delta_distance / 2.0 * math.sin(action.middle_degree)
-
-        if abs(dgxn) > math.pi:
-            dgxn = -dgxn
-        if abs(dgyn) > math.pi:
-            dgyn = -dgyn
-
-        tgn = math.atan2(-dgyn, dgxn)
-        dgn = (dgxn ** 2 + dgyn ** 2) ** (1.0 / 2.0)
-
-        if abs(math.cos(action.middle_degree) - math.cos(state.th)) > 1.0:
-            # it means that x dimensions of the action vector and distance_human vector is not the same
-            if dhx < 0:
-                dhxn = dhx - self.delta_distance / 2.0 * math.cos(action.middle_degree)
-            else:
-                dhxn = dhx + self.delta_distance / 2.0 * math.cos(action.middle_degree)
-        else:   # then they are on the same side. then a step towards that side decreases the distance
-            if dhx < 0:
-                dhxn = dhx + self.delta_distance / 2.0 * math.cos(action.middle_degree)
-            else:
-                dhxn = dhx - self.delta_distance / 2.0 * math.cos(action.middle_degree)
-
-        if abs(math.sin(action.middle_degree) - math.sin(state.th)) > 1.0:
-            # they are not on the same side with y axis
-            dhyn = dhy - self.delta_distance / 2.0 * math.sin(action.middle_degree)
-        else:   # then they are on the same side. then a step towards that side decreases the distance
-            dhyn = dhy + self.delta_distance / 2.0 * math.sin(action.middle_degree)
-                
-        if abs(dhxn) > math.pi:
-            dhxn = -dhxn
-        if abs(dhyn) > math.pi:
-            dhyn = -dhyn
-
-        thn = math.atan2(-dhyn, dhxn)
-        dhn = (dhxn ** 2 + dhyn ** 2) ** (1.0 / 2.0)
-
-        # print(dgxn, dgyn, dhxn, dhyn)
-        # print('dgn:', dgn, ' tgn:', tgn, ' dhn:', dhn, 'thn: ', thn)
+    def transition(self, env, state, action):
+        env.unwrapped.state = [state.x, state.v]
+        next_state, _, _, _ = env.step([action.force])
 
         state_prob_dist = np.zeros(len(self.state_list))
 
         # This part is specific to the deterministic environments
-        state_prob_dist[self.find_closest_state(State(dgn, tgn, dhn, thn))] = 1
+        closest = self.find_closest_state(State(next_state[0], next_state[1]))
+        state_prob_dist[closest] = 1
         return state_prob_dist
 
     def find_closest_state(self, state):
-        dg_ind = tg_ind = dh_ind = th_ind = -1
-        dg_found = tg_found = dh_found = th_found = False
+        min_ind = -1
+        min_dist = np.inf
         for i in range(len(self.state_list)):
-            if th_ind == -1 and state.th <= self.state_list[i].th:
-                if i == 0:
-                    th_ind = 0
-                else:
-                    th_ind = i if np.abs(state.th - self.state_list[i].th) < \
-                                  np.abs(state.th - self.state_list[i-1].th) else (i-1)  # discretizing to the closest
-                th_found = True
-            if dh_ind == -1 and state.dh <= self.state_list[i].dh:
-                if i == 0:
-                    dh_ind = 0
-                else:
-                    dh_ind = i if np.abs(state.dh - self.state_list[i].dh) < \
-                                  np.abs(state.dh - self.state_list[i-1].dh) else (i-1)
-                    dh_found = True
-            if tg_ind == -1 and state.tg <= self.state_list[i].tg:
-                if i == 0:
-                    tg_ind = 0
-                else:
-                    tg_ind = i if np.abs(state.tg - self.state_list[i].tg) < \
-                                  np.abs(state.tg - self.state_list[i-1].tg) else (i-1)
-                tg_found = True
-            if dg_ind == -1 and state.dg <= self.state_list[i].dg:
-                if i == 0:
-                    dg_ind = 0
-                else:
-                    dg_ind = i if np.abs(state.dg - self.state_list[i].dg) < \
-                                  np.abs(state.dg - self.state_list[i-1].dg) else (i-1)
-                dg_found = True
+            dist = state.distance(self.state_list[i])
+            if dist < min_dist:
+                min_dist = dist
+                min_ind = i
 
-            if dg_found and tg_found and dh_found and th_found:
-                break
+        return min_ind
 
-        # if not found, field is discretized into the last cell
-        if not dg_found:
-            dg_ind = len(self.state_list)-1
-        if not tg_found:
-            tg_ind = len(self.state_list)-1
-        if not dh_found:
-            dh_ind = len(self.state_list)-1
-        if not th_found:
-            th_ind = len(self.state_list)-1
-
-        s = State(distance_goal=self.state_list[int(dg_ind)].dg, theta_goal=self.state_list[int(tg_ind)].tg,
-                  distance_human=self.state_list[int(dh_ind)].dh, theta_human=self.state_list[int(th_ind)].th)
-
-        for i in range(len(self.state_list)):
-            if s.is_equal(self.state_list[i]):
-                return i
-
-        print('Error: ')
-        print_state(s)
-        raise ValueError('Environment.find_closest_state failed to find the matching state')
-
-    # This returns the closest index for the check_element at the check_array
-    # bisect_left uses binary search
-    def closest_index(self, check_element, check_array):
-        pos = bisect_left(check_array, check_element)
-        if pos == 0:
-            return pos
-        if pos == len(check_array):
-            return pos - 1
-        before = check_array[pos - 1]
-        after = check_array[pos]
-        if after - check_element < check_element - before:
-            return pos
-        else:
-            return pos - 1
-
-    def calculate_max_distance(self):
-        return ((self.start_point.x - self.goal_point.x) ** 2 +
-                (self.start_point.y - self.goal_point.y) ** 2) ** (1.0 / 2.0)
-
-    # Creates a linear array with states enumerated
-    # enumeration is like: 00001 - 00002 - 00003 .... 0010 - 0011 - 0011 -...
     def save_states(self, file_name):
         print('+ Environment.save_states()')
         np.save(file_name, np.asarray(self.state_list))
 
-    # save actions next to the states
     def save_actions(self, file_name):
         print('+ Environment.save_actions()')
         np.save(file_name, np.asarray(self.action_list))
@@ -215,34 +87,16 @@ class Environment(object):
         nof_states = len(self.state_list)
         transition_mat = np.zeros([nof_states, len(self.action_list), nof_states], dtype=float)  # T[s][a][s']
 
-        # Testing ################################################################
-        # s = a = 0
-        # print('****************state-action-transition***************')
-        # print_state(self.state_list[s])
-        # print_action(self.action_list[a])
-        # test = self.transition(self.state_list[s], self.action_list[a])
-        # for i in range(len(test)):
-        #     if test[i] > 0:
-        #         print(i, ": ", test[i])
-        #
-        # for i in range(10):
-        #     s = int(random.random()*len(self.state_list))
-        #     a = int(random.random()*len(self.action_list))
-        #     print('current state:')
-        #     print_state(self.state_list[s])
-        #     print('current action:')
-        #     print_action(self.action_list[a])
-        #     test = self.transition(self.state_list[s], self.action_list[a])
-        #     for j in range(len(test)):
-        #         if test[j] > 0:
-        #             print('new state is:')
-        #             print_state(self.state_list[j])
-        # Testing ################################################################
+        env = gym.make('MountainCarContinuous-v0')
+        # env = wrappers.Monitor(env, './tmp/', force=True, video_callable=lambda episode_id: episode_id % vid_ep == 0)
+        env.reset()
 
         for i in range(nof_states):
             for j in range(len(self.action_list)):
-                transition_mat[i, j, :] = self.transition(self.state_list[i], self.action_list[j])
+                transition_mat[i, j, :] = self.transition(env, self.state_list[i], self.action_list[j])
+                env.reset()
 
+        env.close()
         np.save(file_name, transition_mat)
         print('- Environment.save_transitions()')
 
@@ -253,8 +107,8 @@ class Environment(object):
 
 
 def print_state(s):
-        print('dg: {0}, tg: {1}, dh: {2}, th: {3}'.format(s.dg, s.tg, s.dh, s.th))
+        print('x: {0}, v: {1}'.format(s.x, s.v))
 
 
 def print_action(a):
-    print('mid_deg: {0}'.format(a.middle_degree))
+    print('force: {0}'.format(a.force))
