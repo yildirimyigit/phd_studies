f7e565f1f70dff246dae51deb9b5c4cd07c0beda
yigit <yigyil@gmail.com>
2019-06-09 16:44:32 +0300

    Environment specific addition: I needed a list of fields of states for convenience

--- s3/python_ws/dme_irl_on_mc/env.py
+++ s3/python_ws/dme_irl_on_mc/env.py
@@ -10,6 +10,7 @@ class IRLMDP:
     def __init__(self, path='data/'):
         self.path = path
         self.states, self.actions, self.transition = self.create_env()
+        self.state_list = self.get_state_list()
         self.gamma = 0.9
         # TODO: start and goal states
         self.start_id = self.get_start_state()
@@ -31,75 +32,17 @@ class IRLMDP:
 
         return min_ind
 
+    # Methods specific to MC.Cont. environment
     def get_start_state(self):
         return self.find_closest_state(State(0, 0))
 
     def get_goal_state(self):
         return self.find_closest_state(State(0.45, 0.055))
 
-    # This method returns a new state for a given action, according to this in
-    # initialize_states() states array is initialized for each dimension
-    # def step(self, sid, aid):
-    #     new_state = self.take_step(sid, aid)
-    #     rew = self.reward(sid)
-    #     is_terminal = is_goal(sid)
-    #     return new_state, rew, is_terminal
-    #
-    # def take_step(self, sid, aid):
-    #     state = self.states[sid]
-    #     action = self.actions[aid]
-    #     dhx = state.dh * np.cos(state.th)
-    #     dhy = state.dh * np.sin(state.th)
-    #     dgx = state.dg * np.cos(state.tg)
-    #     dgy = state.dg * np.sin(state.tg)
-    #
-    #     # dgyn stands for new distance goal (y), the same for the rest of the variables as well
-    #     dgxn = dgx - self.delta_distance / 2.0 * np.cos(action.middle_degree)
-    #     dgyn = dgy - self.delta_distance / 2.0 * np.sin(action.middle_degree)
-    #     tgn = np.arctan(dgyn / dgxn)
-    #     dgn = (dgxn ** 2 + dgyn ** 2) ** (1.0 / 2.0)
-    #
-    #     dhxn = dhx - self.delta_distance * np.cos(action.middle_degree)
-    #     dhyn = dhy - self.delta_distance * np.sin(action.middle_degree)
-    #     thn = np.arctan(dhyn / dhxn)
-    #     if thn < 0:
-    #         thn = np.pi + thn  # when the degree between people are negative
-    #     dhn = (dhxn ** 2 + dhyn ** 2) ** (1.0 / 2.0)
-    #
-    #     new_sid = self.get_state_index(thn, dhn, tgn, dgn)
-    #
-    #     return self.states[new_sid]
-
-    # def get_state_index(self, thn, dhn, tgn, dgn):
-    #     # thn_index = closest_index(thn, self.th_arr)
-    #     # dhn_index = closest_index(dhn, self.dh_arr)
-    #     # tgn_index = closest_index(tgn, self.tg_arr)
-    #     # dgn_index = closest_index(dgn, self.dg_arr)
-    #     #
-    #     # dhlen = len(self.dh_arr)
-    #     # tglen = len(self.tg_arr)
-    #     # dglen = len(self.dg_arr)
-    #
-    #     # tgeff = dglen
-    #     # dheff = tgeff * tglen
-    #     # theff = dhlen * dheff
-    #     #
-    #     # state_index = theff*thn_index + dheff*dhn_index + tgeff*tgn_index + dgn_index
-    #
-    #     state_index = 0
-    #     return state_index
-
-    # R(s)
-    # def reward(self, state):
-    #     if is_goal(state):
-    #         rew = 100
-    #     else:
-    #         rew = -0.1  # The cost of a non-goal step
-    #         if 0 < state.dh < 4:
-    #             rew -= 10/state.dh**2
-    #         elif state.dh == 0:
-    #             rew = -100
-    #     return rew
+    def get_state_list(self):
+        states = []
+        for s in self.states:
+            states.append([s.x, s.v])
 
 # Given state is goal or not
 # def is_goal(state):
