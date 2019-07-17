9d329429a03c77e83025d5c5ac74a679fff47f2d
yigit <yigyil@gmail.com>
2019-07-17 17:56:55 +0300

    Started creating the Objectworld MDP

--- s3/python_ws/dme_irl_on_mc/env.py
+++ s3/python_ws/dme_irl_on_mc/env.py
@@ -12,11 +12,10 @@ class IRLMDP:
         self.states, self.actions, self.transition = self.create_env()
         self.state_list = self.get_state_list()
         self.gamma = 0.9
-        # TODO: start and goal states
         self.start_id = self.get_start_state()
         self.goal_id = self.get_goal_state()
 
-    # returns previously generated states and actions
+    # returns previously generated states, actions and transitions
     def create_env(self):
         return np.load(self.path + 'states.npy', allow_pickle=True), \
                np.load(self.path + 'actions.npy', allow_pickle=True), \
@@ -49,3 +48,53 @@ class IRLMDP:
 # Given state is goal or not
 # def is_goal(state):
 #     return state.dg == 0
+
+
+class Objectworld:
+    def __init__(self, dim=8, stochasticity=0.0, nof_colors=10):  # TODO: stochasticity is 0.3 by definition
+        self.dim = dim
+        self.stochasticity = stochasticity
+        self.nof_actions = 5    # by definition
+        self.gamma = 0.9
+        self.nof_colors = nof_colors
+
+        self.states = self.actions = self.transition = np.empty()
+
+        self.create_env()
+        self.start_id = self.get_start_state()
+        self.goal_id = self.get_goal_state()
+
+    # returns previously generated states, actions and transitions
+    def create_env(self):
+        state_list = []
+        action_list = []
+        transition_matrix = []
+
+
+
+        self.states = np.asarray(state_list)
+        self.actions = np.asarray(action_list)
+        self.transition = np.asarray(transition_matrix)
+
+    def find_closest_state(self, state):
+        min_ind = -1
+        min_dist = np.inf
+        for i in range(len(self.states)):
+            dist = state.distance(self.states[i])
+            if dist < min_dist:
+                min_dist = dist
+                min_ind = i
+
+        return min_ind
+
+    # Methods specific to MC.Cont. environment
+    def get_start_state(self):
+        return self.find_closest_state(State(0, 0))
+
+    def get_goal_state(self):
+        return self.find_closest_state(State(0.45, 0.055))
+
+    class Object:
+        def __init__(self, location=0, color=0):
+            self.location = location
+            self.color = color
