Loss calculation fixed. There still exists an error, though, which I am trying to catch

s3/python_ws/dme_irl_on_mc/agent.py
@@ -2,6 +2,7 @@
   @author: yigit.yildirim@boun.edu.tr
 
   [1]: Kitani 2012, Activity Forecasting
+  [2]: Wulfmeier 2016, Maximum Entropy Deep Inverse Reinforcement Learning
 """
 import numpy as np
 from env import IRLMDP
@@ -17,7 +18,7 @@ class IRLAgent:
     def __init__(self):
         self.env = IRLMDP()
         # initializes nn with random weights
-        self.rew_nn = MyNN(nn_arch=(2, 400, 300, 1), acts=[sigm, sigm, linear])
+        self.rew_nn = MyNN(nn_arch=(2, 64, 32, 1), acts=[sigm, sigm, linear])
         self.state_rewards = np.empty(len(self.env.states))
 
         # self.state_id = self.env.start_id
@@ -28,7 +29,7 @@ class IRLAgent:
         # Creating the output directory for the individual run
         os.makedirs(self.output_directory_path)
 
-        self.vi_loop = 3000
+        self.vi_loop = 1000
         self.v = np.empty((len(self.env.states), self.vi_loop), dtype=float)
         self.q = np.empty((len(self.env.states), len(self.env.actions)), dtype=float)
         self.advantage = np.empty((len(self.env.states), len(self.env.actions)), dtype=float)
@@ -260,8 +261,9 @@ class IRLAgent:
         self.emp_fc = cumulative_emp_fc
         self.plot_emp_fc('empfc')
 
-    def exp_fc(self):   # expected feature counts
-        return np.matmul(self.esvc.T, self.env.state_list)
+    # def exp_fc(self):   # expected feature counts
+    #     # return np.matmul(self.esvc.T, self.env.state_list)
+    #     return self.esvc
 
     def policy(self, sid, aid):
         return np.exp(self.q[sid][aid] - self.v[sid, -1])   # last column in the v matrix

s3/python_ws/dme_irl_on_mc/dme.py
@@ -20,20 +20,22 @@ class DME:
         self.irl_agent = IRLAgent()
         self.iter_count = 10000
 
-        self.losses = np.zeros((self.iter_count, len(self.irl_agent.emp_fc)))
-        self.euler_losses = np.zeros(self.iter_count)
+        # self.losses = np.zeros((self.iter_count, len(self.irl_agent.emp_fc)))
+        self.cumulative_dists = np.zeros(self.iter_count)
 
         # #######################################################################
         # create the directory to be used for plotting for rewards
-        self.reward_path = self.irl_agent.env.path + 'figures/reward/' + str(int(time.time()))
+        self.reward_path = self.irl_agent.output_directory_path + 'reward/'
+        self.loss_path = self.irl_agent.output_directory_path + 'loss/'
         os.makedirs(self.reward_path)
+        os.makedirs(self.loss_path)
         # #######################################################################
 
     def run(self):
         state_array = np.asarray(self.irl_agent.env.state_list)
 
-        lr = 1e-5
-        decay = 3e-9
+        lr = 1e-3
+        decay = 3e-7
 
         for i in range(self.iter_count):
             print('--- Iteration {0} ---'.format(i))
@@ -55,15 +57,17 @@ class DME:
             print('Duration-- back: {0}, forward: {1}'.format(t1-t0, t2-t1))
 
             # calculate loss and euler distance to [0,0, ..., 0] which we want loss to be
-            loss = self.irl_agent.emp_fc - self.irl_agent.exp_fc()
-            euler_loss = np.power(np.sum(np.power(loss, 2)), 0.5)
+            # loss = self.irl_agent.emp_fc - self.irl_agent.exp_fc()  # FAULTY exp_fc calculation
+            diff = self.irl_agent.emp_fc - self.irl_agent.esvc
+            dist = np.abs(diff)
 
             lr = np.maximum(lr - decay, 1e-10)
-            self.irl_agent.rew_nn.backprop_diff(euler_loss, state_array, self.irl_agent.state_rewards, lr, momentum=0.8)
+            self.irl_agent.rew_nn.backprop_diff(dist, state_array, self.irl_agent.state_rewards, lr, momentum=0.5)
 
-            self.losses[i] = loss
-            self.euler_losses[i] = euler_loss
-            print("Loss:" + str(euler_loss)+"\n")
+            # self.losses[i] = dist
+            self.cumulative_dists[i] = np.sum(dist)
+            print("Distance:" + str(self.cumulative_dists[i])+"\n")
+            self.plot_cumulative_dists(i)
 
     def plot_reward(self, nof_iter):
         dim = int(np.sqrt(len(self.irl_agent.env.state_list)))
@@ -71,8 +75,12 @@ class DME:
 
         hm = sb.heatmap(data)
         fig = hm.get_figure()
-        fig.savefig(self.reward_path + '/' + str(nof_iter) + '.png')
+        fig.savefig(self.reward_path + str(nof_iter) + '.png')
         fig.clf()
+
+    def plot_cumulative_dists(self, i):
+        plt.plot(range(i), self.cumulative_dists[:i])
+        plt.savefig(self.loss_path + str(i) + '.png')
 
     # testing value iteration algorithm of the agent
     def test_backward_pass(self):

s3/python_ws/dme_irl_on_mc/neural_network.py
@@ -1,7 +1,7 @@
 """
   author: yigit.yildirim@boun.edu.tr
 
-  Note: The math and the code belongs to me but I borrowed some minor ideas from Ryan Harris'
+  Note: The math and the code belongs to me but I borrowed some ideas from Ryan Harris'
   explanations on YouTube: https://www.youtube.com/watch?v=XqRUHEeiyCs
 """
 
@@ -91,8 +91,8 @@ class MyNN:
             self.nof_layers = len(nn_arch)  # including the input layer
             self.weights = []
             for (l1, l2) in zip(nn_arch[:-1], nn_arch[1:]):
-                self.weights.append(np.random.uniform(high=0.05, size=(l1+1, l2)) - 0.025)  # +1 for augmenting bias
-                # self.weights.append(np.random.normal(scale=0.2, size=(l1 + 1, l2)))  # +1 for augmenting the bias
+                # self.weights.append(np.random.uniform(high=0.05, size=(l1+1, l2)) - 0.025)  # +1 for augmenting bias
+                self.weights.append(np.random.normal(scale=0.2, size=(l1 + 1, l2)))  # +1 for augmenting the bias
                 self.last_weight_deltas.append(np.zeros((l1+1, l2)))
 
         else:  # TODO: shape calculation missing for now
@@ -147,7 +147,7 @@ class MyNN:
         diff, total_error = sum_error(y, y_hat)
         for i in reversed(range(self.nof_layers-1)):
             if i == self.nof_layers - 2:  # output of the last layer
-                deltas.append(diff*self.activations[i](y_hat, der=True))  # diff of each instance in the batch
+                deltas.append(diff * self.activations[i](y_hat, der=True))  # diff of each instance in the batch
                 # is multiplied by the corresponding derivative
             else:
                 last_delta = deltas[-1]
@@ -172,6 +172,7 @@ class MyNN:
         return total_error
 
     def backprop_diff(self, diff, x, y_hat, lr=0.15, momentum=0.5):
+        diff = np.reshape(diff, np.shape(y_hat))
         batch_size = len(x)
         deltas = []
         for i in reversed(range(self.nof_layers-1)):