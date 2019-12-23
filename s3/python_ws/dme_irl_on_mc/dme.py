28e5dc41248adafe1ed4ce591184e6f26cb53250
yigit <yigyil@gmail.com>
2019-12-23 17:35:06 +0300

    forgot to correct the previous unnecessary test. correcting now

--- s3/python_ws/dme_irl_on_mc/dme.py
+++ s3/python_ws/dme_irl_on_mc/dme.py
@@ -60,8 +60,7 @@ class DME:
             # calculate loss and euler distance to [0,0, ..., 0] which we want loss to be
             # loss = self.irl_agent.emp_fc - self.irl_agent.exp_fc()  # FAULTY exp_fc calculation
             diff = self.irl_agent.emp_fc - self.irl_agent.esvc
-            # dist = np.power(diff, 2)
-            dist = np.abs(diff)
+            dist = np.power(diff, 2)
 
             lr = np.maximum(lr - decay, 1e-10)
             self.irl_agent.backpropagation_batch(dist, lr)
