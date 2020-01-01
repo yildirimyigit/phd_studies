Many important changes; there was an error in the calculation in the forward pass which is fixed, changed the softmax into max in the backward pass. Also some minor changes.

s3/python_ws/dme_irl_on_mc/agent.py
@@ -6,20 +6,19 @@
 """
 import numpy as np
 from env import IRLMDP
-from neural_network import MyNN, sigm, linear, gaussian
+from neural_network import MyNN, sigm, linear, relu
 
 import sys
 import seaborn as sb
 import os
 import time
-from tqdm import tqdm
 
 
 class IRLAgent:
     def __init__(self):
         self.env = IRLMDP()
         # initializes nn with random weights
-        self.rew_nn = MyNN(nn_arch=(2, 128, 128, 1), acts=[gaussian, sigm, linear])
+        self.rew_nn = MyNN(nn_arch=(2, 128, 64, 32, 1), acts=[sigm, relu, sigm, linear])
         self.state_rewards = np.empty(len(self.env.states), dtype=float)
         self.initialize_rewards()
 
@@ -29,12 +28,11 @@ class IRLAgent:
         # Creating the output directory for the individual run
         os.makedirs(self.output_directory_path)
 
-        self.vi_loop = 100
+        self.vi_loop = 150
         self.normalized_states = np.empty(len(self.env.states))
         self.v = np.empty((len(self.env.states), self.vi_loop), dtype=float)
         self.q = np.empty((len(self.env.states), len(self.env.actions)), dtype=float)
         self.advantage = np.empty((len(self.env.states), len(self.env.actions)), dtype=float)
-        self.current_policy = np.empty((len(self.env.states), len(self.env.actions)), dtype=float)
         self.fast_policy = np.empty((len(self.env.states), len(self.env.actions)), dtype=float)
         self.esvc = np.empty(len(self.env.states), dtype=float)
         self.esvc_mat = np.empty((len(self.env.states), self.vi_loop), dtype=float)
@@ -50,61 +48,36 @@ class IRLAgent:
 
         self.mc_normalized_states()
 
-    ###############################################
-    # [1]
-    def backward_pass(self):
-        # print("+ IRLAgent.backward_pass")
-
-        self.v[:] = -sys.float_info.max
-
-        for i in tqdm(range(self.vi_loop-1)):
-            self.v[self.env.goal_id, i] = 0
-
-            for s in range(len(self.env.states)):
-                if s == self.env.goal_id:
-                    continue
-                state_reward = self.state_rewards[s]
-                for a in range(len(self.env.actions)):
-                    self.q[s][a] = state_reward + np.matmul(self.env.transition[s][a], self.v[:, i])
-
-            # v  = softmax_a Q
-            q = np.exp(self.q)
-            max_q = np.max(q, axis=1)   # max of each row: max q value over actions for each state
-            nonzero_ids = np.where(max_q != 0)
-            self.v[nonzero_ids, i+1] = max_q[nonzero_ids] / np.sum(q[nonzero_ids], axis=1)
-
-        print('')
-
-        # self.plot_policy()
-        # print("\n- IRLAgent.backward_pass")
+        self.q_file = open(self.output_directory_path + 'q.txt', "a+")
 
     ###############################################
     # [1]: Calculates the policy using an approximate version of Value Iteration
-    def fast_backward_pass(self):
+    def fast_backward_pass(self, ind):
         # print("+ IRLAgent.backward_pass")
 
         v = np.ones((len(self.env.states), 1)) * -sys.float_info.max
         q = np.zeros((len(self.env.states), len(self.env.actions)))
 
-        for i in range(self.vi_loop*2-1):
+        for i in range(self.vi_loop-1):
             v[self.env.goal_id] = 0
             for s in range(len(self.env.states)):
                 q[s, :] = np.matmul(self.env.transition[s, :, :], v).T + self.state_rewards[s]
 
             # v = softmax_a q
-            # one problem:
-            # when np.sum(np.exp(q), axis=1) = 0, division by 0. In this case v = 0
-            expq = np.exp(q)
-            sumexpq = np.sum(expq, axis=1)
-            nonzero_ids = np.where(sumexpq != 0)
-            zero_ids = np.where(sumexpq == 0)
-            v[nonzero_ids, 0] = np.exp(np.max(q[nonzero_ids], axis=1))/sumexpq[nonzero_ids]
-            v[zero_ids, 0] = -sys.float_info.max
+            # one problem: when np.sum(np.exp(q), axis=1) = 0, division by 0. In this case v = 0
+            # expq = np.exp(q)
+            # sumexpq = np.sum(expq, axis=1)
+            # nonzero_ids = np.where(sumexpq != 0)
+            # zero_ids = np.where(sumexpq == 0)
+            # v[nonzero_ids, 0] = np.exp(np.max(q[nonzero_ids], axis=1))/sumexpq[nonzero_ids]
+            # v[zero_ids, 0] = -sys.float_info.max
+            v = np.max(q, axis=1)
 
             if i % 20 == 19:
                 print('\rBackward Pass: {}'.format((i + 1)), end='')
 
         print('')
+        # self.save_q(q, ind)
         v[self.env.goal_id] = 0
         # current MaxEnt policy:
         self.advantage = q - np.reshape(v, (len(self.env.states), 1))
@@ -115,72 +88,8 @@ class IRLAgent:
         # self.plot_policy()
         # print("\n- IRLAgent.backward_pass")
 
-    ##############################################################################################
-    # [1]: Calculates fast_policy using an approximate version of Value Iteration algorithm
-    def ffast_backward_pass(self):
-        # print("+ IRLAgent.backward_pass")
-
-        v = np.ones((len(self.env.states), 1)) * -sys.float_info.max
-
-        for i in range(self.vi_loop-1):
-            v[self.env.goal_id] = 0
-            q = [(np.matmul(self.env.transition[s, :, :], v).T + self.state_rewards[s])[0]
-                 for s in range(len(self.env.states))]
-
-            # q = np.reshape(tq, (len(self.env.states), len(self.env.actions)))
-            # v = softmax_a q
-            # one problem:
-            # when np.sum(np.exp(q), axis=1) = 0, division by 0. In this case v = 0
-            expq = np.exp(q)
-            sumexpq = np.sum(expq, axis=1)
-            nonzero_ids = np.where(sumexpq != 0)
-            zero_ids = np.where(sumexpq == 0)
-            v[nonzero_ids, 0] = np.exp(np.max(np.array(q)[nonzero_ids], axis=1))/sumexpq[nonzero_ids]
-            v[zero_ids, 0] = -sys.float_info.max
-
-            print('\rBackward Pass: {}'.format((i+1)), end='')
-
-        v[self.env.goal_id] = 0
-        # current MaxEnt policy:
-        self.advantage = q - np.reshape(v, (len(self.env.states), 1))
-        self.fast_policy = np.exp(self.advantage)
-
-        # self.plot_policy()
-        # print("\n- IRLAgent.backward_pass")
-
     ###############################################
-    # [1]
-    # Simulates the propagation of the policy
-    def forward_pass(self):  # esvc: expected state visitation count
-        # print("+ IRLAgent.forward_pass")
-
-        # #######################################################################
-        # create the directory to be used for plotting
-        # since forward will be called on multiple times, I use system time here
-        path = self.env.path + 'figures/forward_pass/' + str(int(time.time()))
-        os.makedirs(path)
-        # #######################################################################
-
-        self.esvc_mat[:] = 0
-        self.esvc_mat[self.env.start_id, :] = 1
-        # for loop_ctr in range(10):
-        for loop_ctr in tqdm(range(self.vi_loop-1)):
-            self.esvc_mat[self.env.goal_id][loop_ctr] = 0
-            esvc_unnorm = np.zeros(len(self.env.states))
-            for j in range(len(self.env.states)):
-                esvc_unnorm[j] = self.calc_esvc_unnorm(j, loop_ctr)
-
-            # normalization to calculate the frequencies
-            self.esvc_mat[:, loop_ctr + 1] = esvc_unnorm/sum(esvc_unnorm)
-            print('\rForward Pass: {}'.format((loop_ctr+1)), end='')
-            self.plot_esvc_mat()
-        self.esvc = np.sum(self.esvc_mat, axis=1)
-        self.plot_esvc(path, 'esvc', self.esvc)
-        print('')
-        # print("\n- IRLAgent.forward_pass")
-
-    ###############################################
-    # [1]
+    # [1]: Simulates the propagation of the policy
     def fast_forward_pass(self):  # esvc: expected state visitation count
         # print("+ IRLAgent.forward_pass")
 
@@ -196,7 +105,6 @@ class IRLAgent:
 
         print('')
         self.esvc = np.sum(self.esvc_mat, axis=1)/self.vi_loop  # averaging over <self.vi_loop> many examples
-        # self.esvc = self.esvc_mat[:, -1]
         # self.plot_esvc(path, 'esvc', self.esvc)
         # print("\n- IRLAgent.forward_pass")
 
@@ -225,13 +133,12 @@ class IRLAgent:
     ###############################################
 
     def ffast_calc_esvc_unnorm(self):
-        esvc = [self.esvcind(i) for i in range(len(self.env.states))]
-
-        return esvc
+        esvc_arr = [self.esvcind(i) for i in range(len(self.env.states))]
+        return esvc_arr
 
     def esvcind(self, ind):
-        ret = np.matmul((self.env.transition[:, :, ind] * self.fast_policy).T, self.esvc_mat[:, self.cur_loop_ctr])
-        return np.sum(ret)
+        esvc = np.matmul((self.env.transition[:, :, ind] * self.fast_policy).T, self.esvc_mat[:, self.cur_loop_ctr])
+        return np.sum(esvc)
 
     ###############################################
 
@@ -249,15 +156,8 @@ class IRLAgent:
         self.emp_fc = cumulative_emp_fc
         self.plot_emp_fc('empfc')
 
-    # def exp_fc(self):   # expected feature counts
-    #     # return np.matmul(self.esvc.T, self.env.state_list)
-    #     return self.esvc
-
     def policy(self, sid, aid):
         return np.exp(self.q[sid][aid] - self.v[sid, -1])   # last column in the v matrix
-
-    def set_current_policy(self):
-        self.current_policy = np.exp(self.q - np.reshape(self.v[:, -1], (len(self.env.states), 1)))
 
     def reward(self, state):
         return self.rew_nn.forward(np.asarray([state.x, state.v]))
@@ -314,3 +214,20 @@ class IRLAgent:
         fig = hm.get_figure()
         fig.savefig(path + "esvc_" + str(i) + '.png')
         fig.clf()
+
+    def save_q(self, q, ind):
+        self.q_file.write(str(ind) + "\n")
+        self.q_file.write("[")
+
+        for i in range(len(self.env.states)):
+            self.q_file.write("[")
+            for j in range(len(self.env.actions)):
+                self.q_file.write(str(q[i, j]))
+                if j != len(self.env.actions) - 1:
+                    self.q_file.write(", ")
+            self.q_file.write("]")
+            if i != len(self.env.states) - 1:
+                self.q_file.write(", ")
+
+        self.q_file.write("] \n\n")
+        self.q_file.flush()

s3/python_ws/dme_irl_on_mc/dme.py
@@ -32,12 +32,13 @@ class DME:
         # #######################################################################
         self.rewards_file = open(self.reward_path + 'rewards.txt', "a+")
         self.esvc_file = open(self.esvc_path + 'esvc.txt', "a+")
+        self.policy_file = open(self.irl_agent.output_directory_path + 'policy.txt', "a+")
         # #######################################################################
 
     def run(self):
 
-        lr = 1e-1
-        decay = 1e-7
+        lr = 1e-3
+        decay = 1e-8
 
         for i in range(self.iter_count):
             print('--- Iteration {0} ---'.format(i))
@@ -51,11 +52,13 @@ class DME:
 
             # solve mdp wrt current reward
             t0 = time.time()
-            self.irl_agent.fast_backward_pass()
+            self.irl_agent.fast_backward_pass(i)
             t1 = time.time()
             self.irl_agent.fast_forward_pass()   # calculate irl.esvc to use it in calculation of irl.exp_fc
             t2 = time.time()
             print('Duration-- back: {0}, forward: {1}'.format(t1-t0, t2-t1))
+
+            # self.save_policy(i)
 
             # calculate loss and euler distance to [0,0, ..., 0] which we want loss to be
             # loss = self.irl_agent.emp_fc - self.irl_agent.exp_fc()  # FAULTY exp_fc calculation
@@ -66,7 +69,7 @@ class DME:
             lr = np.maximum(lr - decay, 1e-10)
             self.irl_agent.backpropagation_batch(dist, lr)
 
-            self.cumulative_dists[i] = np.sum(dist) * 1e6
+            self.cumulative_dists[i] = np.sum(dist)
             print("Distance:" + str(self.cumulative_dists[i])+"\n")
             self.plot_cumulative_dists(i)
             self.irl_agent.plot_esvc_mat(self.esvc_path, i)
@@ -116,6 +119,23 @@ class DME:
 
         self.esvc_file.write("] \n")
         self.esvc_file.flush()
+
+    def save_policy(self, ind):
+        self.policy_file.write(str(ind) + "\n")
+        self.policy_file.write("[")
+
+        for i in range(len(self.irl_agent.env.states)):
+            self.policy_file.write("[")
+            for j in range(len(self.irl_agent.env.actions)):
+                self.policy_file.write(str(self.irl_agent.fast_policy[i, j]))
+                if j != len(self.irl_agent.env.actions) - 1:
+                    self.policy_file.write(", ")
+            self.policy_file.write("]")
+            if i != len(self.irl_agent.env.states) - 1:
+                self.policy_file.write(", ")
+
+        self.policy_file.write("] \n\n")
+        self.policy_file.flush()
 
     def plot_reward2(self, nof_iter):
         # plt.ylim(-0.2, 0.2)