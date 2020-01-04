7e8f41db85d2f5f030c9508c2639d40ca333c080
yigit <yigyil@gmail.com>
2020-01-04 10:36:25 +0300

    pytorch implementation (maybe, someday?)

--- dmev2/dme.py
+++ dmev2/dme.py
@@ -1,3 +1,11 @@
 """
   @author: yigit.yildirim@boun.edu.tr
 """
+
+import torch
+from irl_agent import IRLAgent
+
+
+class DME:
+    def __init__(self):
+        self.agent = IRLAgent()
--- dmev2/irl_agent.py
+++ dmev2/irl_agent.py
@@ -1,3 +1,11 @@
 """
   @author: yigit.yildirim@boun.edu.tr
 """
+
+import torch
+
+
+class IRLAgent:
+    def __init__(self):
+        self.env = IRLMDP()
+        self.exp_fc = torch.zeros()
