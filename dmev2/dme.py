e318f09f43dd75c9ac9257fdcaafb27a6435ecb8
yigit <yigyil@gmail.com>
2020-01-18 16:03:55 +0300

    pytorch-first attempts

--- dmev2/dme.py
+++ dmev2/dme.py
@@ -8,4 +8,13 @@ from irl_agent import IRLAgent
 
 class DME:
     def __init__(self):
-        self.agent = IRLAgent()
+        self.cuda = torch.cuda.is_available()
+        self.iter_count = 50000
+
+        if self.cuda:
+            dev = torch.device("cuda")
+        else:
+            dev = torch.device("cpu")
+
+        self.error = torch.zeros(self.iter_count, device=dev)
+        self.agent = IRLAgent(dev)
---
+++ dmev2/test_pytorch.py
@@ -0,0 +1,8 @@
+"""
+  @author: yigit.yildirim@boun.edu.tr
+"""
+
+import torch
+
+x = torch.rand(3, dtype=torch.double)
+print(x)
