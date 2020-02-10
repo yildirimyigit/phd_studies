7c7dc0ee8253861ec33333bce3dcd43dd5ed76c2
yigit <yigyil@gmail.com>
2020-02-10 15:32:27 +0300

    minor change probably. Can't remember right now and diff seems so hard :)

--- s3/python_ws/dme_irl_on_mc/gridworld_process_data.py
+++ s3/python_ws/dme_irl_on_mc/gridworld_process_data.py
@@ -6,18 +6,38 @@ import numpy as np
 from gridworld_mdp import GridworldMDP
 
 
+def produce_demonstrations(mdp, policy, num):
+    dems = []
+    goal = mdp.get_goal_state()
+
+    for i in range(num):
+        state = 0
+        dem = []
+        while state != goal:
+            action = np.random.choice(np.arange(mdp.num_actions), 1, p=policy[state])
+            new_state = np.where(mdp.transitions[state, action, :] == 1)[0][0]
+            dem.append([state, action])
+            state = new_state
+
+        dems.append(dem)
+    return dems
+
+
 # Loads previously recorded demonstrations to create trajectories
 # :param e Environment object
 def generate_trajectories(mdp):
     trajectories = []
     trajectories_of_ids = []
 
-    policy = [[[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 2], [14, 2], [22, 2], [30, 2], [38, 2], [46, 2]],
-              [[0, 2], [8, 2], [16, 2], [24, 2], [32, 2], [40, 2], [48, 1], [49, 1], [50, 1], [51, 1], [52, 1], [53, 1]],
-              [[0, 1], [1, 2], [9, 1], [10, 2], [18, 1], [19, 2], [27, 1], [28, 2], [36, 1], [37, 2], [45, 1], [46, 2]],
-              [[0, 2], [8, 1], [9, 2], [17, 2], [25, 2], [33, 1], [34, 1], [35, 2], [43, 1], [44, 1], [45, 1], [46, 2]]]
+# #policy = [[[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 2], [14, 2], [22, 2], [30, 2], [38, 2], [46, 2]],
+# #          [[0, 2], [8, 2], [16, 2], [24, 2], [32, 2], [40, 2], [48, 1], [49, 1], [50, 1], [51, 1], [52, 1], [53, 1]],
+# #          [[0, 1], [1, 2], [9, 1], [10, 2], [18, 1], [19, 2], [27, 1], [28, 2], [36, 1], [37, 2], [45, 1], [46, 2]],
+# #          [[0, 2], [8, 1], [9, 2], [17, 2], [25, 2], [33, 1], [34, 1], [35, 2], [43, 1], [44, 1], [45, 1], [46, 2]]]
 
-    for ind_policy in policy:
+    num_dems = 20
+    demonstrations = produce_demonstrations(mdp, np.load(mdp.env_path + 'policy.npy'), num_dems)
+
+    for ind_policy in demonstrations:
         trajectory = []
         trajectory_of_ids = []
         for state_action in ind_policy:
